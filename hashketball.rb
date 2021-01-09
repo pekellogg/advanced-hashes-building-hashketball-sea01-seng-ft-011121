95e7afcc52521f985d0aa6040e250cf21e0eeb68
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-09 10:28:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,19 @@
 def game_hash
-  {:home => {:team_name => "Insert Team Name Here", :colors => ["color1", "color2"], :players => [{},{}] },
+  {
+    :home => {:team_name => "Insert Team Name Here",
+    :colors => ["color1", "color2"],
+    :players => [
+    {:player_name => "stat"},
+      {:number => "stat"},
+      {:shoe => "stat"},
+      {:points => "stat"},
+      {:rebounds => "stat"},
+      {:assists => "stat"},
+      {:steals => "stat"},
+      {:blocks => "stat"},
+      {:slam_dunks => "stat"}
+      ]
+    },
    :away => {:team_name => "Insert Team Name Here", :colors => ["color1", "color2"], :players => [{},{}] }
   }
 end
