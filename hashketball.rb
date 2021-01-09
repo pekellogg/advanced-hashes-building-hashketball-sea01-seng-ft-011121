6683c9fa1350d59c5f9ea10db66f3e0b169204ad
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-09 10:30:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,20 +1,35 @@
 def game_hash
   {
-    :home => {:team_name => "Insert Team Name Here",
-    :colors => ["color1", "color2"],
-    :players => [
-    {:player_name => "stat"},
-      {:number => "stat"},
-      {:shoe => "stat"},
-      {:points => "stat"},
-      {:rebounds => "stat"},
-      {:assists => "stat"},
-      {:steals => "stat"},
-      {:blocks => "stat"},
-      {:slam_dunks => "stat"}
-      ]
+    :home =>
+      {:team_name => "Insert Team Name Here",
+        :colors => ["color1", "color2"],
+        :players => [
+          {:player_name => "stat"},
+          {:number => "stat"},
+          {:shoe => "stat"},
+          {:points => "stat"},
+          {:rebounds => "stat"},
+          {:assists => "stat"},
+          {:steals => "stat"},
+          {:blocks => "stat"},
+          {:slam_dunks => "stat"}
+        ]
     },
-   :away => {:team_name => "Insert Team Name Here", :colors => ["color1", "color2"], :players => [{},{}] }
+   :away =>
+    {:team_name => "Insert Team Name Here",
+      :colors => ["color1", "color2"],
+      :players => [
+        {:player_name => "stat"},
+        {:number => "stat"},
+        {:shoe => "stat"},
+        {:points => "stat"},
+        {:rebounds => "stat"},
+        {:assists => "stat"},
+        {:steals => "stat"},
+        {:blocks => "stat"},
+        {:slam_dunks => "stat"}
+     ]
+ }
   }
 end
 
